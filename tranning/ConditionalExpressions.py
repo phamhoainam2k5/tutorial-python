7b27d754777eef144d67ef90dd6f038e759a8185
phamhoainam2k5 <phahoainam2005@gmail.com>
2024-09-23 22:09:56 +0700

    commit day 23/09/2024

---
+++ tranning/ConditionalExpressions.py
@@ -0,0 +1,9 @@
+number = 6
+a=5
+b=4
+
+result = "EVEN" if number % 2 == 0 else "ODD"
+max_number = a if a > b else b
+min_number = a if a < b else b
+
+print(min_number)
---
+++ tranning/StringMethods.py
@@ -0,0 +1,15 @@
+# name = input("Enter your full name: ")
+phone_number = input("Enter your phone number #: ")
+
+# result = len(name)
+# result = name.find("N")
+# result = name.rfind("a")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+# phone_number = phone_number.count("-")
+phone_number = phone_number.replace("-", "")
+
+print(phone_number)
